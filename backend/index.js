729399ee0eb97f3e8bbf6a235acd3d3dc8b4d354
Abhay-989 <abhaychouhan8799@gmail.com>
2025-03-28 18:28:31 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -147,43 +146,26 @@ app.use(bodyParser.json());
 //   res.send("Done!");
 // });
 
-// app.get("/addPositions", async (req, res) => {
-//   let tempPositions = [
+// app.get("/addOrders", async (req, res) => {
+//   let tempOrders = [
 //     {
-//       product: "CNC",
-//       name: "EVEREADY",
-//       qty: 2,
-//       avg: 316.27,
-//       price: 312.35,
-//       net: "+0.58%",
-//       day: "-1.24%",
-//       isLoss: true,
+//       name: "INFY",
+//       qty:1,
+//       price: 1555.45,
+//      mode:"Buy",
 //     },
-//     {
-//       product: "CNC",
-//       name: "JUBLFOOD",
-//       qty: 1,
-//       avg: 3124.75,
-//       price: 3082.65,
-//       net: "+10.04%",
-//       day: "-1.35%",
-//       isLoss: true,
-//     },
+  
 //   ];
 
-//   tempPositions.forEach((item) => {
-//     let newPosition = new PositionsModel({
-//       product: item.product,
+//   tempOrders.forEach((item) => {
+//     let newOrders = new OrdersModel({
 //       name: item.name,
 //       qty: item.qty,
-//       avg: item.avg,
-//       price: item.price,
-//       net: item.net,
-//       day: item.day,
-//       isLoss: item.isLoss,
+//       price:item.price,
+//       mode:item.mode,
 //     });
 
-//     newPosition.save();
+//     newOrders.save();
 //   });
 //   res.send("Done!");
 // });
